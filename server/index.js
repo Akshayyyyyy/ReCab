da2168a9cfd03f1a6976e6ee2946da23430b8e9d
Akshayyyyyy <88049485+Akshayyyyyy@users.noreply.github.com>
2025-06-28 14:45:31 +0530

    Update index.js cors url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "https://recab.onrender.com",
+  origin: "https://recab-front.onrender.com",
   credentials: true,
 }));
 
